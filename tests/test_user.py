580d45d3a7318aed4c4623aca6e0ca46e695d4f0
Andrei <andrej.daxnov@bk.ru>
2025-05-10 15:50:50 +0300

    first commit

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,10 +1,8 @@
 from fastapi.testclient import TestClient
-
 from src.main import app
 
 client = TestClient(app)
 
-# Существующие пользователи
 users = [
     {
         'id': 1,
@@ -26,16 +24,45 @@ def test_get_existed_user():
 
 def test_get_unexisted_user():
     '''Получение несуществующего пользователя'''
-    pass
+    response = client.get("/api/v1/user", params={'email': 'non.existing@mail.com'})
+    assert response.status_code == 404
+    assert response.json() == {"detail": "User not found"}
 
 def test_create_user_with_valid_email():
     '''Создание пользователя с уникальной почтой'''
-    pass
+    new_user = {
+        "name": "Sergey Sergeev",
+        "email": "s.sergeev@mail.com"
+    }
+    response = client.post("/api/v1/user", json=new_user)
+    assert response.status_code == 201
+    user_id = response.json()
+    assert isinstance(user_id, int)
+
+    get_response = client.get("/api/v1/user", params={'email': new_user['email']})
+    assert get_response.status_code == 200
+    assert get_response.json()['email'] == new_user['email']
 
 def test_create_user_with_invalid_email():
     '''Создание пользователя с почтой, которую использует другой пользователь'''
-    pass
+    response = client.post("/api/v1/user", json={
+        "name": "Duplicate",
+        "email": users[0]["email"]
+    })
+    assert response.status_code == 409
+    assert response.json() == {"detail": "User with this email already exists"}
 
 def test_delete_user():
     '''Удаление пользователя'''
-    pass
+
+    new_user = {
+        "name": "To Delete",
+        "email": "to.delete@mail.com"
+    }
+    client.post("/api/v1/user", json=new_user)
+
+    delete_response = client.delete("/api/v1/user", params={"email": new_user["email"]})
+    assert delete_response.status_code == 204
+
+    get_response = client.get("/api/v1/user", params={"email": new_user["email"]})
+    assert get_response.status_code == 404
